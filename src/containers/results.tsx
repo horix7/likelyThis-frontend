2de492ff737c5f3aefada4862adf27a5f0d68de9
horix7 <horix77777@gmail.com>
2021-06-08 22:17:29 +0200

    results page

---
+++ src/containers/results.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import '../App.css';
+import SearcField from "../components/forms/searchFields"
+import HomeNav from "../components/navigation/homeNav"
+
+export default function Home() {
+  return (
+      <Fragment>
+      <HomeNav />
+        <p>Succes Page</p>
+      </Fragment>
+
+  );
+}
+
