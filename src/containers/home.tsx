012dc36acdf3c5f56a5d38a9598ee1b978a48c94
horix7 <horix77777@gmail.com>
2021-06-08 19:01:16 +0200

    adding home page

---
+++ src/containers/home.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import '../App.css';
+import SearcField from "../components/forms/searchFields"
+import HomeNav from "../components/navigation/homeNav"
+
+export default function Home() {
+  return (
+      <Fragment>
+      <HomeNav />
+      <header className="App-header">
+        <SearcField />
+      </header>
+      </Fragment>
+
+  );
+}
+
