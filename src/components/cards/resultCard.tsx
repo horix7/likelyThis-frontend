b13f96d4531f282d05517fa9f48cfaaca84570c0
horix7 <horix77777@gmail.com>
2021-06-09 11:39:59 +0200

    un likekly

--- src/components/cards/resultCard.tsx
+++ src/components/cards/resultCard.tsx
@@ -1,24 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Card } from "@material-ui/core";
 import "./result.css"
 import { motion } from "framer-motion";
 import { IconContext  } from "react-icons";
-import { SiVerizon } from "react-icons/si";
+import { GiCheckMark } from "react-icons/gi";
+import { BsExclamation  } from "react-icons/bs";
 
 export default function ResultCard() {
+    const [state, setState] = useState({
+        likely: false
+    })
   return (
       <Fragment>
     <motion.div animate={{ scale: 1.2,   transition: { duration: 1 , repeat: 0}}}> 
       <div className="resultBox">
         <Card style={{border: "none"}}>
-            <h1 className="result-text">your are <span className="likely">likely</span> to transfer computer science II  </h1>
+            <h1 className="result-text">your are {state.likely ?  <span className="likely">likely</span>  :  <span className="not-likely">not likely </span> }to transfer computer science II  </h1>
            
         </Card>
-        <IconContext.Provider value={{ color: "green", className: "result-icon" }}>
+       {state.likely ?  <IconContext.Provider value={{ color: "green", className: "result-icon" }}>
                 <motion.div whileHover={{ scale: 1.5, transition: { duration: 0.2}}} > 
-                    <SiVerizon />
+                    <GiCheckMark />
                  </motion.div>
-             </IconContext.Provider>
+             </IconContext.Provider> :  <IconContext.Provider value={{ color: "red", className: "result-icon" }}>
+                <motion.div whileHover={{ scale: 1.5, transition: { duration: 0.2}}} > 
+                    <BsExclamation />
+                 </motion.div>
+             </IconContext.Provider>}
         </div>
         </motion.div>
 
